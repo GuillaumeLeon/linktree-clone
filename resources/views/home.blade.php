ed65dc354e1ef7d490cabdb9fe96fcf76e305a30
Guillaume Leon <guillaume.leon2000@gmail.com>
2020-10-29 23:35:04 +0100

    Minor changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">This is a simple linktree-clone</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    <div class="card-body">
+                        You can connect or register right now or you can type the username of user to get his link <br>
+                        e.g : <a href="https://linktree-clone.guillaumeleon.xyz/zappellin">
+                            https://linktree-clone.guillaumeleon.xyz/zappellin
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,24 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+  if (Auth::check()) {
+    return redirect('/dashboard/links');
+  } else {
     return view('home');
+  }
 });
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
+
+//Route::redirect('/home', '/dashboard/links');
+//Route::redirect('/', '/dashboard/links');
+
 
 Auth::routes();
 // http://127.0.0.1:8000/dashboard
 Route::group(['middleware' => 'auth', 'prefix' => 'dashboard'], function () {
 
-    Route::get('/links', 'LinkController@index');
+    Route::get('/links', 'Linkcontroller@index');
     Route::get('/links/new', 'LinkController@create');
     Route::post('/links/new', 'LinkController@store');
     Route::get('/links/{link}', 'LinkController@edit');
